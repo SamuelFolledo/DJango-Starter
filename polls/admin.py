54f9b48ad866653219230d0f527f80a6e59b70d4
Samuel Folledo <samuelfolledo@gmail.com>
2019-12-07 18:33:34 -0800

    [add] updated admin.py to show Choices that Questions has in the admin page [add] TabularInLine stack for the Choices so it wont take that much spaces in height

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline): #StackedInLine will have it stack, TabularInLine is cleaner
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline] #puts it in the same field
+
+admin.site.register(Question, QuestionAdmin)
+
+# admin.site.register(Choice) #will create another thing, but if we want it in the same line (if they are related)
+# then we will want an StackedInLine
